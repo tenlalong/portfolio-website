738d61a25615ff24cb581474591c0015abc3b3d2
tenlalong <long26800@gmail.com>
2023-07-04 15:42:05 -0400

    Fix another bug about NameAnimation

--- src/pages/components/header/NameAmimation.js
+++ src/pages/components/header/NameAmimation.js
@@ -16,44 +16,19 @@ Currently looking for Software Engineer opportunities.`;
 
     const animateName = () => {
       if (index < fullName.length - 1) {
-        if (fullName[index] === " " && fullName[index + 1] === " ") {
-          // Handle consecutive spaces by replacing them with a single space
-          setName((prevName) => prevName + " ");
-          index++;
-        } else {
-          setName((prevName) => prevName + fullName[index]);
-          index++;
-        }
+        setName((prevName) => prevName + fullName[index]);
+        index++;
       } else {
         clearInterval(intervalId);
-        setTimeout(clearName, 1000); // Wait for 1 second before clearing the name
-      }
-    };
-
-    const clearName = () => {
-      const nameLength = name.length;
-      if (nameLength > 0) {
-        setName((prevName) => prevName.slice(0, nameLength - 1));
-      } else {
-        clearInterval(clearIntervalId);
-        setTimeout(animateName, 1000); // Wait for 1 second before animating the name again
       }
     };
 
     intervalId = setInterval(animateName, 100); // Adjust the interval duration for the desired speed
-    clearIntervalId = setInterval(clearName, 100); // Interval for clearing the name
 
     return () => {
       clearInterval(intervalId);
-      clearInterval(clearIntervalId);
     };
   }, []);
-
-  // const lines = name.split("\n").map((line, index) => (
-  //   <div key={index} style={{ marginBottom: "50px", fontSize: "30px" }}>
-  //     {line}
-  //   </div>
-  // ));
 
   const lines = name.split("\n").map((line, index) => {
     let fontSize = "30px"; // Default font size
